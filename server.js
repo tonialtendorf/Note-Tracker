939c08930899cc79570a9da42a7babe2e5cdff2e
tonialtendorf <tvohnoutka05@gmail.com>
2023-03-02 16:43:44 -0600

    final changes

--- server.js
+++ server.js
@@ -1,41 +1,89 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uniqid = require('uniqid');
+const util = require('util');
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
 const app = express();
 
-
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-//gets saved notes
-app.get("/api/notes", (reg, res) => {
-  res.sendFile(path.join(__dirname, "/db/db.json"))
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+const readFromFile = util.promisify(fs.readFile);
+
+
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination 
+ *  @param {object} content 
+ *  @returns {void} 
+ */
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content 
+ *  @param {string} file 
+ *  @returns {void} 
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+// GET Route for retrieving notes
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-//adds new notes
-app.post("/api/notes", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-  const newNote = req.body;
-  newNote.id = uniqid();
-  notes.push(newNote);
-  fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-  res.json(notes);
+// POST Route for a new note
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a new note`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
 });
 
-//html call
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
